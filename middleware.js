813f3d06177989e4d7781f83948b38d4d3a400e7
AbdulRehman387 <abdulrehman03082007@gmail.com>
2025-01-01 13:54:46 +0500

    updated middleware

--- middleware.js
+++ middleware.js
@@ -1,19 +1,25 @@
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 // Define paths for single and double protection
 const singleProtectedPaths = ['/cart', '/checkout, /orders'];
 const doubleProtectedPaths = ['/dashboard/orders', '/dashboard/products, /dashboard/users, /dashboard/messages'];
 
 export async function middleware(req) {
-  // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const localCookie = req.cookies.has("__Secure-next-auth.session-token")
-  const vercelCookie = req.cookies.has("next-auth.session-token")
-  const isLogin = localCookie || vercelCookie
+  const isLogin = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production", // Ensure secure cookies in production
+  });
+  // const localCookie = req.cookies.has("__Secure-next-auth.session-token")
+  // const vercelCookie = req.cookies.has("next-auth.session-token")
+  // const isLogin = localCookie || vercelCookie
 
   const { pathname } = req.nextUrl;
-
+  
   // Check for single protected pages (user must be logged in)
   if (singleProtectedPaths.includes(pathname)) {
+    console.log(isLogin, " this is is login idahshcoahgij");
     if (!isLogin) {
       return NextResponse.redirect(new URL('/login', req.url));
     }
