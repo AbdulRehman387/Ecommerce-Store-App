2444d587b8bc048442a2c76a79858e4f2bb76742
AbdulRehman387 <abdulrehman03082007@gmail.com>
2025-01-01 14:06:51 +0500

    debugging middleware

--- middleware.js
+++ middleware.js
@@ -6,7 +6,6 @@ const singleProtectedPaths = ['/cart', '/checkout, /orders'];
 const doubleProtectedPaths = ['/dashboard/orders', '/dashboard/products, /dashboard/users, /dashboard/messages'];
 
 export async function middleware(req) {
-  console.log("Cookies in Production are these:", req.cookies.getAll());
   const isLogin = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
@@ -15,11 +14,12 @@ export async function middleware(req) {
   // const localCookie = req.cookies.has("__Secure-next-auth.session-token")
   // const vercelCookie = req.cookies.has("next-auth.session-token")
   // const isLogin = localCookie || vercelCookie
-
+  
   const { pathname } = req.nextUrl;
   
   // Check for single protected pages (user must be logged in)
   if (singleProtectedPaths.includes(pathname)) {
+    console.log("Cookies in Production are these:", req.cookies.getAll());
     console.log(isLogin, " this is is login idahshcoahgij");
     if (!isLogin) {
       return NextResponse.redirect(new URL('/login', req.url));
