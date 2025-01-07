ac24d4eb69e080e38e8c05afdf4e459aea908b0d
AbdulRehman387 <abdulrehman03082007@gmail.com>
2025-01-08 00:38:47 +0500

    changed product ui

--- app/collections/[collection]/page.jsx
+++ app/collections/[collection]/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 import React from 'react'
-import { MdArrowForward } from 'react-icons/md'
 import { useEffect } from 'react'
-import { getSession } from 'next-auth/react'
 import { toast } from 'react-toastify'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { PiShoppingCartSimpleFill } from 'react-icons/pi';
+import { BiSolidWallet } from 'react-icons/bi';
 
 const Page = ({ params }) => {
+  const { data: session, state } = useSession()
   const [products, setProducts] = React.useState([])
   useEffect(() => {
     fetch('/api/products')
@@ -17,21 +20,28 @@ const Page = ({ params }) => {
   }, [])
 
   const onClickHandler = async (id) => {
-    toast.success('Item added to cart', {
-      position: "top-right",
-      autoClose: 1000, // Closes after 3 seconds
-    });
-    const session = await getSession()
-    const response = await fetch('/api/cart', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        userId: session?.user?.id,
-        productId: id
-      }),
-    });
+    if (session) {
+      toast.success('Item added to cart', {
+        position: "top-right",
+        autoClose: 1000, // Closes after 3 seconds
+      });
+      const response = await fetch('/api/cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userId: session?.user?.id,
+          productId: id
+        }),
+      });
+    }
+    else {
+      toast.error('Please Login first!', {
+        position: "top-right",
+        autoClose: 1000, // Closes after 3 seconds
+      });
+    }
   }
 
   return (
@@ -46,19 +56,29 @@ const Page = ({ params }) => {
             products?.map((item) => {
               return (
                 <div key={item.id} className="bg-white w-[300px] overflow-hidden max-w-sm py-5 text-[#5a5757] mobile:w-[45vw] mobile:py-2">
-                  <div className="relative flex justify-center">
-                    <img className="h-[200px]" src={item.image} alt="Product Image" />
+                  <Link href={`/products/${item.id}`} className="relative flex justify-center">
+                    <img className="h-[200px] mobile:h-[150px]" src={item.image} alt="Product Image" />
                     <div className="absolute bottom-0 left-0 bg-red-500 text-white px-3 py-0.5 m-2 rounded-full text-sm">Sale
                     </div>
-                  </div>
+                  </Link>
                   <div className="p-4 mobile:p-1">
-                    <h3 className="text-lg font-medium mb-2 mobile:text-sm">Silicon Power 256GB SSD</h3>
+                    <h3 className="text-lg font-medium mb-2 mobile:text-sm">{item.title.slice(0, 24) + "..."}</h3>
                     <div className="flex items-center justify-between">
-                      <span className="font-bold text-lg mobile:text-sm">${item.price}</span>
-                      <div className='flex justify-center items-center cursor-pointer group'>
-                        <button onClick={() => onClickHandler(item.id)} className='mobile text-sm group-hover:text-gray-900 group-active:text-gray-400'>Add to cart</button>
-                        <MdArrowForward className='text-2xl mobile:text-xl group-hover:translate-x-1.5 group-hover:scale-110 transition-all duration-100' />
-                      </div>
+                      <span className="font-bold text-xl text-gray-800 mobile:text-sm">${item.price}</span>
+                    </div>
+                    <div className='flex flex-col justify-center items-center w-full gap-y-2 mt-4 mobile:relative mobile:bottom-2'>
+                      <button onClick={() => onClickHandler(item.id)} className='bg-white text-black text-base font-medium px-4 py-2 w-full rounded-sm hover:scale-[1.01] duration-150 border border-black mobile:text-sm mobile:py-1.5'>
+                        <div className='flex justify-center items-center gap-x-2'>
+                          <PiShoppingCartSimpleFill className='text-black text-xl mobile:text-lg' />
+                          <p>Add to cart</p>
+                        </div>
+                      </button>
+                      <Link href={`/checkout/${item.id}`} className='bg-black text-white text-base font-medium px-4 py-2 w-full rounded-sm hover:scale-[1.01] duration-150 mobile:text-sm mobile:py-1.5'>
+                        <div className='flex justify-center items-center gap-x-2'>
+                          <BiSolidWallet className='text-white text-xl mobile:text-lg' />
+                          <p>Buy now</p>
+                        </div>
+                      </Link>
                     </div>
                   </div>
                 </div>
--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -4,11 +4,11 @@ import { useState } from 'react'
 import { BiSolidWallet } from "react-icons/bi";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
 import Link from 'next/link';
-import { MdArrowForward } from 'react-icons/md';
 import { toast } from 'react-toastify';
-import { getSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 const Page = ({ params }) => {
+  const {data:session, state} = useSession()
   const [products, setProducts] = useState([{
     id: "",
     title: "",
@@ -37,21 +37,28 @@ const Page = ({ params }) => {
   }, [])
 
 const onClickHandler = async (id) => {
-    toast.success('Item added to cart', {
-      position: "top-right",
-      autoClose: 1000, // Closes after 3 seconds
-    });
-    const session = await getSession()
-    const response = await fetch('/api/cart', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        userId: session?.user?.id,
-        productId: id
-      }),
-    });
+    if (session) {
+      toast.success('Item added to cart', {
+        position: "top-right",
+        autoClose: 1000, // Closes after 3 seconds
+      });
+      const response = await fetch('/api/cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userId: session?.user?.id,
+          productId: id
+        }),
+      });
+    }
+    else{
+      toast.error('Please Login first!', {
+        position: "top-right",
+        autoClose: 1000, // Closes after 3 seconds
+      });
+    }
   }
 
   return (
@@ -67,13 +74,13 @@ const onClickHandler = async (id) => {
           <p className='text-xl font-semibold text-gray-800 mt-3'>Description:</p>
           <p className='text-lg font-medium text-gray-500 leading-[23px]'>{product.description}</p>
           <div className='flex flex-col justify-center items-center w-full gap-y-3 mt-4'>
-            <button onClick={()=>onClickHandler(params.id)} className='bg-black text-white text-xl font-semibold p-x-4 py-3 w-full rounded-sm hover:scale-[1.01] duration-150'>
+            <button onClick={()=>onClickHandler(params.id)} className='bg-black text-white text-xl font-medium p-x-4 py-3 w-full rounded-sm hover:scale-[1.01] duration-150'>
               <div className='flex justify-center items-center gap-x-2'>
                 <PiShoppingCartSimpleFill className='text-white text-3xl' />
                 <p>Add to cart</p>
               </div>
             </button>
-            <Link href={`/checkout/${params.id}`} className='bg-black text-white text-xl font-semibold p-x-4 py-3 w-full rounded-sm hover:scale-[1.01] duration-150'>
+            <Link href={`/checkout/${params.id}`} className='bg-black text-white text-xl font-medium p-x-4 py-3 w-full rounded-sm hover:scale-[1.01] duration-150'>
               <div className='flex justify-center items-center gap-x-2'>
                 <BiSolidWallet className='text-white text-3xl' />
                 <p>Buy now</p>
@@ -93,18 +100,28 @@ const onClickHandler = async (id) => {
               return (
                 <div key={item.id} className="bg-white w-[300px] overflow-hidden max-w-sm py-5 text-[#5a5757] mobile:w-[45vw] mobile:py-2">
                   <div className="relative flex justify-center">
-                    <Link href={`/products/${item.id}`}><img className="h-[200px]" src={item.image} alt="Product Image" /></Link>
+                    <Link href={`/products/${item.id}`}><img className="h-[200px] mobile:h-[150px]" src={item.image} alt="Product Image" /></Link>
                     <div className="absolute bottom-0 left-0 bg-red-500 text-white px-3 py-0.5 m-2 rounded-full text-sm">Sale
                     </div>
                   </div>
                   <div className="p-4 mobile:p-1">
-                    <h3 className="text-lg font-medium mb-2 mobile:text-sm">Silicon Power 256GB SSD</h3>
+                    <h3 className="text-lg font-medium mb-2 mobile:text-sm">{item.title.slice(0, 24) + "..."}</h3>
                     <div className="flex items-center justify-between">
-                      <span className="font-bold text-lg mobile:text-sm">${item.price}</span>
-                      <div className='flex justify-center items-center cursor-pointer group'>
-                        <button onClick={()=>onClickHandler(item.id)} className='mobile text-sm group-hover:text-gray-900 group-active:text-gray-400'>Add to cart</button>
-                        <MdArrowForward className='text-2xl mobile:text-xl group-hover:translate-x-1.5 group-hover:scale-110 transition-all duration-100' />
-                      </div>
+                      <span className="font-bold text-xl text-gray-800 mobile:text-sm">${item.price}</span>
+                    </div>
+                    <div className='flex flex-col justify-center items-center w-full gap-y-2 mt-4 mobile:relative mobile:bottom-2'>
+                      <button onClick={() => onClickHandler(item.id)} className='bg-white text-black text-base font-medium px-4 py-2 w-full rounded-sm hover:scale-[1.01] duration-150 border border-black mobile:text-sm mobile:py-1.5'>
+                        <div className='flex justify-center items-center gap-x-2'>
+                          <PiShoppingCartSimpleFill className='text-black text-xl mobile:text-lg' />
+                          <p>Add to cart</p>
+                        </div>
+                      </button>
+                      <Link href={`/checkout/${item.id}`} className='bg-black text-white text-base font-medium px-4 py-2 w-full rounded-sm hover:scale-[1.01] duration-150 mobile:text-sm mobile:py-1.5'>
+                        <div className='flex justify-center items-center gap-x-2'>
+                          <BiSolidWallet className='text-white text-xl mobile:text-lg' />
+                          <p>Buy now</p>
+                        </div>
+                      </Link>
                     </div>
                   </div>
                 </div>
